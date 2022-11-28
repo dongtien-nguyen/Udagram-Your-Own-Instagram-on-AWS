897330eb1e5eed53c2c9fa1f13427f640ad75ef8
tien.nguyendong <dongtien.nguyen93@gmail.com>
2022-11-28 19:21:04 +0700

    fix code review

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
@@ -28,8 +28,8 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
   /**************************************************************************** */
-  app.get( "/filteredimage", async ( req, res ) => {
-    const image_url = req.query.image_url;
+  app.get( "/filteredimage", async ( req: Request, res: Response ) => {
+    const image_url:string = req.query.image_url;
     if (image_url) {
       filterImageFromURL(image_url).then((filteredpath) => {
         res.sendFile(filteredpath, function (err) {
